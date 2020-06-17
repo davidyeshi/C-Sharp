83116363ca7fa67d00665376d0764bc6725a44a2
Yeshi <david_yeshi@outlook.com>
2020-06-17 17:38:43 +1000

    Delete Api Call from js

--- BookListRazor/BookListRazor/wwwroot/js/bookList.js
+++ BookListRazor/BookListRazor/wwwroot/js/bookList.js
@@ -22,7 +22,11 @@ function loadDataTable() {
                         <div class="text-center">
                             <a href="/BookList/Edit?id=${data}" class="btn btn-success text-white" style="cursor: pointer; width: 100px"> Edit </a>
                             &nbsp;
-                            <a class="btn btn-danger text-white" style="cursor: pointer; width: 100px"> Delete </a>
+                            <a 
+                                onclick=Delete('api/book?id=${data}')
+                                class="btn btn-danger text-white" 
+                                style="cursor: pointer; 
+                                width: 100px"> Delete </a>
                         </div>
                     `;
                 }, 
@@ -33,5 +37,30 @@ function loadDataTable() {
             "emptyTable": "no data found"
         },
         "width": "100%"
+    })
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
     })
 }
